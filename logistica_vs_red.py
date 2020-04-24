29173473c0e2a650564e2e832e3afb91e610b8e1
jerck1 <johncafre@gmail.com>
2020-04-24 12:38:11 -0500

    pushing

--- logistica_vs_red.py
+++ logistica_vs_red.py
@@ -20,7 +20,7 @@ from sklearn.metrics import confusion_matrix
 #%matplotlib inline
 
 
-# In[11]:
+# In[2]:
 
 
 import sklearn.datasets
@@ -29,13 +29,13 @@ import sklearn.neural_network
 import sklearn.model_selection
 
 
-# In[2]:
+# In[3]:
 
 
 from sklearn.metrics import f1_score
 
 
-# In[3]:
+# In[4]:
 
 
 files=glob.glob("./Stocks/*.txt")
@@ -43,13 +43,13 @@ print(np.shape(files))
 print(files[:20])
 
 
-# In[4]:
+# In[5]:
 
 
 # Solamente uso las columnas  x="high" y="nombre del archivo". Ejm
 
 
-# In[5]:
+# In[6]:
 
 
 data = pd.read_csv("{}".format(files[0]),delimiter=",")
@@ -58,20 +58,20 @@ print(labels)
 print(np.shape(data))
 
 
-# In[6]:
+# In[7]:
 
 
 #las dimensiones son 1249 (cada compañía)
 print(labels[0])
 
 
-# In[7]:
+# In[8]:
 
 
 #tomo todos los elementos que no tengan celdas vacías
 
 
-# In[8]:
+# In[ ]:
 
 
 n_max=1200#number of files taken
@@ -102,21 +102,21 @@ print(np.shape(X))
 print(np.shape(Y))
 
 
-# In[20]:
+# In[ ]:
 
 
 # las categorías que esperamos obtener son los meses del año en el orden en que aparece el array Y.
 # En este caso tenemos 7 meses:
 
 
-# In[9]:
+# In[ ]:
 
 
 print(len(date))
 print((Y))
 
 
-# In[12]:
+# In[ ]:
 
 
 # Vamos a hacer un split training test
@@ -124,7 +124,7 @@ scaler = StandardScaler()
 x_train, x_test, y_train, y_test = train_test_split(X, Y, train_size=0.5)
 
 
-# In[13]:
+# In[ ]:
 
 
 x_train = scaler.fit_transform(x_train)
@@ -136,7 +136,7 @@ print(np.shape(x_test))
 print(np.shape(y_train))
 
 
-# In[53]:
+# In[ ]:
 
 
 #Number of neurons
@@ -148,13 +148,13 @@ f1_s_train=[]
 f1_s_test=[]
 
 
-# In[29]:
+# In[ ]:
 
 
 # para una capa
 
 
-# In[30]:
+# In[ ]:
 
 
 for i in n:
@@ -168,10 +168,10 @@ for i in n:
     #print('Loss', mlp.loss_)#entropía relativa, divergencia
 
 
-# In[31]:
+# In[ ]:
 
 
-plt.figure(1,figsize = (10,10))
+plt.figure(1,figsize = (8,10))
 plt.subplot(1,2,1)
 plt.plot(n,loss)
 plt.xlabel('n neurons')
@@ -179,18 +179,19 @@ plt.ylabel('loss')
 plt.subplot(1,2,2)
 plt.plot(n,f1_s_train,label="train")
 plt.plot(n,f1_s_test,label="test")
+plt.hlines(0.9833968253968254,min(n),max(n),label="logistic model")
 plt.xlabel('n neurons')
 plt.ylabel('f_1 score')
-plt.savefig("loss_f1.png")
+plt.savefig("f1_1capa.png")
 
 
-# In[22]:
+# In[ ]:
 
 
 # dos capa
 
 
-# In[58]:
+# In[ ]:
 
 
 #Number of neurons
@@ -199,17 +200,17 @@ n2=np.arange(2,8)
 #loss
 loss=[]
 #f1_score
-f1_s_train=np.zeros((len(n1)+1,len(n2)+1))
-f1_s_test=np.zeros((len(n1)+1,len(n2)+1))
+f1_s_train2=np.zeros((len(n1)+1,len(n2)+1))
+f1_s_test2=np.zeros((len(n1)+1,len(n2)+1))
 
 
-# In[48]:
+# In[ ]:
 
 
 #np.shape(f1_s_train)
 
 
-# In[62]:
+# In[ ]:
 
 
 for i in n1:
@@ -219,25 +220,25 @@ for i in n1:
                                                max_iter=200)
         mlp.fit(x_train, y_train)
         loss=np.append(loss,mlp.loss_)
-        f1_s_train[i-1,j-2]=sklearn.metrics.f1_score(y_train, mlp.predict(x_train), average='macro')
-        f1_s_test[i-1,j-2]=sklearn.metrics.f1_score(y_test, mlp.predict(x_test), average='macro')
+        f1_s_train2[i-1,j-2]=sklearn.metrics.f1_score(y_train, mlp.predict(x_train), average='macro')
+        f1_s_test2[i-1,j-2]=sklearn.metrics.f1_score(y_test, mlp.predict(x_test), average='macro')
         #print('Loss', mlp.loss_)#entropía relativa, divergencia
 
 
-# In[84]:
+# In[ ]:
 
 
 # Regresión logísitica utilizando l1
 
 
-# In[106]:
+# In[ ]:
 
 
 # Turn up tolerance for faster convergence
 train_samples = int(np.shape(Y)[0]*0.5)
 f1_av_1=[]
 #regresión logística sobre los dígitos
-for i in np.log(np.arange(np.e,1000,10)):
+for i in np.log(np.arange(1.1,100,3)):
     clf = LogisticRegression(
         C=i, penalty='l1', solver='saga', tol=0.1)
         #C=50. / train_samples, penalty='l1', solver='saga', tol=0.1)#,multi_class='multinomial'
@@ -246,14 +247,17 @@ for i in np.log(np.arange(np.e,1000,10)):
     f1_av_1=np.append(f1_av_1,f1_score(y_test,y_pred, average='weighted'))
 
 
-# In[108]:
+# In[ ]:
 
 
-plt.scatter(np.log(np.arange(np.e,1000,10)),f1_av_1,label="f1")
-#plt.scatter(np.arange(1,10),f1_av_1,label="f1")
+plt.figure()
+plt.scatter(np.log(np.arange(1.1,100,3)),f1_av_1,label="l1")
+#plt.scatter(np.log(np.arange(1.1,100,3)),f1_av_2,label="l2")
+plt.legend(loc=(1.05,0.25))
+print(f1_av_1[2])
 
 
-# In[96]:
+# In[ ]:
 
 
 for i in n1:
@@ -264,16 +268,22 @@ for i in n1:
     plt.xlabel('n neurons')
     plt.ylabel('loss')
     #plt.subplot(1,7,i)
-    plt.plot(n2,f1_s_train[i,:6],label="train {} redes 1ra capa".format(i))
-    plt.plot(n2,f1_s_test[i,:6],label="test {} redes 1ra capa".format(i))
+    plt.plot(n2,f1_s_train2[i,:6],label="train {} redes 1ra capa".format(i))
+    plt.plot(n2,f1_s_test2[i,:6],label="test {} redes 1ra capa".format(i))
     plt.xlabel('n neurons')
     plt.ylabel('f_1 score')
-plt.hlines(0.85548043,min(n2),max(n2),label="logistic model")
+plt.hlines(0.9833968253968254,min(n2),max(n2),label="logistic model")
 plt.legend(loc=(1.05,0.25))
-    #plt.savefig("loss_f1.png")
+plt.savefig("f1_2doscapas.png")
 
 
 # # Discusión
 
 # * En mi caso los resultados no fueron tan buenos cuando se con las redes neuronales que al aplicar la regresión logística.
-# * 
+# * Los resultados muestran que la cuando hay 3 redes en la primera capa el f1 score tiende a aumentar, lo cual nos indica que debemos tomar 3 redes en la primera capa
+
+# In[ ]:
+
+
+
+
